0d759e958c7c2451411c91e4fa36efddf0242977
Nyasha Perry <nyashaperry2005@yahoo.com>
2021-01-04 00:05:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   smallest_value = 2000
-  smallest_key = nil
+  smallest_key = -9
   name_hash.each do |key, value|
     if value < smallest_value
       smallest_value = value
